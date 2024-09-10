e34a9b466a913bbd4416c48e6fea6024243c68db
Sébastien <sebastien_besse@yahoo.com>
2024-09-10 23:28:51 +0200

    Initial Commit

---
+++ Meme Generator/Meme_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_GeneratorApp.swift
+//  Meme Generator
+//
+//  Created by Sebastien Besse on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Meme_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
