a287053d0dbd184789a5e585f8b6a7ce58b702fe
Sébastien <sebastien_besse@yahoo.com>
2024-09-18 18:12:22 +0200

    Created componants

---
+++ Meme Generator/View/Components/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  Meme Generator
+//
+//  Created by Sebastien Besse on 18/09/2024.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
