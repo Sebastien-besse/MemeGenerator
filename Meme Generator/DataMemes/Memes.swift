08b86a09cf702184f3de466c85da5bc9ca5db830
Sébastien <sebastien_besse@yahoo.com>
2024-09-19 01:42:21 +0200

    Data

--- Meme Generator/DataMemes/Memes.swift
+++ Meme Generator/DataMemes/Memes.swift
@@ -7,4 +7,5 @@
 
 import Foundation
 
+
 var memes = ["memes-bébé", "memes-charlie", "memes-disloyal", "memes-shiba", "memes-black"]
---
+++ Meme Generator/View/Components/PhotoPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPickerView.swift
+//  Meme Generator
+//
+//  Created by Sebastien Besse on 18/09/2024.
+//
+
+import SwiftUI
+
+struct PhotoPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoPickerView()
+}
